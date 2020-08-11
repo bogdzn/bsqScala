d37c575d00ee5ceb36d60cc5549cb8d71eefd90b
bogdan <wontsleep@protonmail.com>
2020-08-11 14:48:48 +0200

    Fix: Added generateIntArray() to load a random board for testing.

--- src/test/scala/bsq/SetSpec.scala
+++ src/test/scala/bsq/SetSpec.scala
@@ -1,12 +1,15 @@
 import bsq._
 import org.scalatest.funsuite.AnyFunSuite
+import scala.util.Random
 
 class SetSpec extends AnyFunSuite {
 
   def generateIntArray(size: Int): Array[Array[Int]] = {
-    var result: Array[Array[Int]] =
-      for (idx <- 0 until size) result :+= Array.fill(size)(0 | 1)
-    result
+    def createSingleArray(size: Int): Array[Int] = {
+      val rand = new Random()
+      Array.fill[Int](size)(0 + rand.nextInt(1))
+    }
+    Array.fill[Array[Int]](size)(createSingleArray(size))
   }
 
   val posOutOfBounds: Square = Square(232, 12, 0)
@@ -22,13 +25,13 @@ class SetSpec extends AnyFunSuite {
     assert(!expectFalse, "Should not be Out of Bounds")
   }
 
-  test("ToNext") {
+  test("toNext") {
     val expectFalse : Option[Square] = posOutOfBounds.toNext(intBoard)
     val expectTrue : Option[Square] = posCorrect.toNext(intBoard)
 
     assert(expectTrue.nonEmpty, "toNext should be ok for position(1, 1).")
-    assert(expectTrue.get.x == 2, "expectTrue.x should be equal to 2.")
-    assert(expectTrue.get.y == 1, "expectTrue.y should be equal to 1.")
+    assert(expectTrue.get.x == 1, "expectTrue.x should be equal to 1.")
+    assert(expectTrue.get.y == 2, "expectTrue.y should be equal to 2.")
     assert(expectFalse.isEmpty, "toNext should fail for position(232, 12)")
   }
 }
