e3adb98f6ab9c94771cb8ee7bd7f5075af5103f3
bogdan <wontsleep@protonmail.com>
2020-08-11 15:55:03 +0200

    Mod: Adding unit tests.

--- src/test/scala/bsq/SetSpec.scala
+++ src/test/scala/bsq/SetSpec.scala
@@ -12,6 +12,14 @@ class SetSpec extends AnyFunSuite {
     Array.fill[Array[Int]](size)(createSingleArray(size))
   }
 
+  def modifyBoard(targetPos: Square, modification: Int, board: Array[Array[Int]]): Array[Array[Int]] = {
+    board(targetPos.x)(targetPos.y) = modification
+    board(targetPos.x - 1)(targetPos.y) = modification
+    board(targetPos.x - 1)(targetPos.y - 1) = modification
+    board(targetPos.x)(targetPos.y - 1) = modification
+    board
+  }
+
   val posOutOfBounds: Square = Square(232, 12, 0)
   val posCorrect: Square = Square(1, 1, 0)
   val intBoard: Array[Array[Int]] = generateIntArray(10)
@@ -26,12 +34,32 @@ class SetSpec extends AnyFunSuite {
   }
 
   test("toNext") {
+    val posAtEndOfLine = Square(1, 9, 0)
+
     val expectFalse : Option[Square] = posOutOfBounds.toNext(intBoard)
     val expectTrue : Option[Square] = posCorrect.toNext(intBoard)
+    val expectTrueAlso : Option[Square] = posAtEndOfLine.toNext(intBoard)
 
     assert(expectTrue.nonEmpty, "toNext should be ok for position(1, 1).")
     assert(expectTrue.get.x == 1, "expectTrue.x should be equal to 1.")
     assert(expectTrue.get.y == 2, "expectTrue.y should be equal to 2.")
     assert(expectFalse.isEmpty, "toNext should fail for position(232, 12)")
+    assert(expectTrueAlso.nonEmpty, "toNext should be ok for position(1, 9).")
+    assert(expectTrueAlso.get.x == 2, "Position realignement error.")
+    assert(expectTrueAlso.get.y == 1, "Position realignement error.")
+  }
+
+  test("isSquare") {
+    val expectTrue = posCorrect.isSquare(modifyBoard(posCorrect, 1, intBoard))
+    val expectFalse = posCorrect.isSquare(modifyBoard(posCorrect, 0, intBoard))
+
+    assert(expectTrue, "Position(1, 1) should find a Square.")
+    assert(!expectFalse, "Position(1, 1) should not find a Square.")
+  }
+
+  test("getSquareSize") {
+    val size = posCorrect.getSquareSize(modifyBoard(posCorrect, 1, intBoard))
+
+    assert(size == 2, "Size should be equal to Two.")
   }
 }
