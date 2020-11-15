c4f49ad30026e4570be6dc140e2eb55b4111f0cd
bogdan <wontsleep@protonmail.com>
2020-11-15 18:22:56 +0100

    Mod: properly refactored everything and fixed toNumberedLine().

--- src/main/scala/bsq/Board.scala
+++ src/main/scala/bsq/Board.scala
@@ -2,48 +2,49 @@ package bsq
 
 case class Board(board: Array[String]) {
 
-  def isValidBoard(): Boolean = {
-    def isValidLine(expectedLength: Int, line: String): Boolean = {
-      if (expectedLength != line.length())
-        false
-      else if (line.exists(char => char != 'o' && char != '.' && char != '\n'))
-        false
-      else true
-    }
+  private def isValidLine(expectedLength: Int, line: String): Boolean = {
+    if (expectedLength != line.length())
+      false
+    else if (line.exists(char => char != 'o' && char != '.' && char != '\n'))
+      false
+    else true
+  }
 
+  private def toNumberedLine(line: String): Array[Int] =
+    line.map(char => if (char == '.') 1 else 0).toArray
+
+  private def updateLine(index: Int, squareSize: Int, line: String): String = {
+    val startOfLine = line.slice(0, index)
+    val endLine =
+      if (squareSize + index < line.length)
+        line.slice(index + squareSize, line.length)
+      else ""
+
+    startOfLine + ("x" * squareSize) + endLine
+  }
+
+  private def isLineWithSquare(sq: Square, lineCount: Int): Boolean =
+    sq.x - lineCount >= 0 && sq.x - lineCount < sq.size
+
+  def isValidBoard(): Boolean = {
     val boardSize = this.board.size
     val expectedLen = if (boardSize == 0) 0 else this.board(0).length()
 
     this.board.filter(str => isValidLine(expectedLen, str)).length == boardSize
   }
 
-  def toIntBoard(): Array[Array[Int]] = {
-    def toNumberedLine(line: String): Array[Int] =
-      line.map(char => if (char == 'o') 1 else 0).toArray
+  def toIntBoard(): Array[Array[Int]] =
     this.board.map(line => toNumberedLine(line)).toArray
-  }
 
   def isEdgeCase(): Boolean =
     (this.board.length == 1 || this.board(0).length == 1)
 
-  def toSolvedMap(bsq: Square): Array[String] = {
-    def addSolvedLine(index: Int, squareSize: Int, line: String): String = {
-      val startOfLine = line.slice(0, index)
-      val endLine =
-        if (squareSize + index < line.length)
-          line.slice(index + squareSize, line.length)
-        else ""
-
-      startOfLine + ("x" * squareSize) + endLine
+  def toSolvedMap(s: Square): Array[String] = {
+    for (idx <- this.board.indices) {
+      if (isLineWithSquare(s, idx))
+        this.board(idx) = updateLine(s.y - s.size + 1, s.size, this.board(idx))
     }
 
-    def isLineWithSquare(bsq: Square, lineCount: Int): Boolean =
-      bsq.x - lineCount >= 0 && bsq.x - lineCount < bsq.size
-
-    for (idx <- this.board.indices)
-      if (isLineWithSquare(bsq, idx))
-        this.board(idx) =
-          addSolvedLine(bsq.y - bsq.size + 1, bsq.size, this.board(idx))
     this.board
   }
 }
